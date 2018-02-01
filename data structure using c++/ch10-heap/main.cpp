9cf56b2f4c5728fe37783ddcd61d2a40eea364bc
pouder-Man <vkdnej91@gmail.com>
2018-02-01 19:02:13 +0900

    add heap sort

--- data structure using c++/ch10-heap/main.cpp
+++ data structure using c++/ch10-heap/main.cpp
@@ -4,11 +4,22 @@
 
 #include "MaxHeap.h"
 
+//heapSort
+void heapSort(int* a, int n){
+    MaxHeap heap;
+    for(int i = 0 ; i < n ; i++){
+        heap.insert(a[i]);
+    }
+
+    for(int i = n-1 ; i>-1 ; i--){
+        a[i] = heap.remove().getKey();
+    }
+}
+
 int main(){
 
     MaxHeap heap;
-
-
+    //insert,remove,print
     heap.insert(10);
     heap.insert(17);
     heap.insert(13);
@@ -27,6 +38,24 @@ int main(){
     cout<<endl;
     heap.remove();
     heap.display();
+    cout<<endl<<endl;
+
+    //heapsort
+    int arr[10];
+    for(int i = 0 ; i < 10 ; i++ )
+        arr[i] = rand()%100;
+
+    cout << "heap sort!!"<<endl;
+    cout << "before sort"<<endl;
+    for(int i = 0 ; i < 10 ; i++ )
+        cout<< arr[i] << " ";
     cout<<endl;
+
+    heapSort(arr,10);
+    cout << "after sort"<<endl;
+    for(int i = 0 ; i < 10 ; i++ )
+        cout<< arr[i] << " ";
+    cout<<endl;
+
     return 0;
 }
